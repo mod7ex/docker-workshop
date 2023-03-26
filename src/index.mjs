acdbd2433972c92e877ed9a2a0e4e5298c90ba95
Mourad EL CADI <mouradface1998@gmail.com>
2023-03-26 14:52:12 +0000

    save

---
+++ src/index.mjs
@@ -0,0 +1,57 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { MongoClient } from "mongodb";
+import bodyParser from "body-parser";
+
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+
+const DbInit = (() => {
+  /**
+   * @type {import('mongodb').Db}
+   **/
+  let db;
+
+  return async () => {
+    if (!db) {
+      const client = new MongoClient("mongodb://modex98:password@localhost:27017");
+      await client.connect();
+      db = client.db("user-account");
+    }
+    return db;
+  };
+})();
+
+const getUsers = async () => {
+  return (await DbInit()).collection("users").find({}).toArray();
+};
+
+const saveUser = async (data) => {
+  const db = await DbInit();
+  return db.collection("users").insertOne(data);
+};
+
+app.get("/", (_, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/", urlencodedParser, async (req, res) => {
+  await saveUser(req.body);
+
+  res.redirect("/");
+});
+
+app.get("/users", async (_, res) => {
+  const users = await getUsers();
+  res.json(users);
+});
+
+app.listen(3000, () => {
+  console.log("App listening on 3000! check http://localhost:3000");
+});
