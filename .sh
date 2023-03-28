7344e9b197ef5633cf625bac8fb95fd395f62175
Mourad EL CADI <mouradface1998@gmail.com>
2023-03-28 17:36:50 +0000

    save

--- .sh
+++
@@ -1,8 +0,0 @@
-# since we have issue with building from docker-compose.yaml file we will build manually
-
-docker build -f DockerFile . -t my_app:latest
-
-echo 'Image built with success'
-
-docker-compose up -d
-
